6850dfa4583bcd6cea068b363c9d6c3056abafec
huixingm <zcynewbsd@163.com>
2020-02-28 14:29:27 +0800

    权限管理

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@ module.exports = {
 	NODE_ENV: '"development"',
 	ENV_CONFIG: '"dev"',
 	//BASE_API: '"https://www.example.com/admin"'
-	BASE_API: '"http://47.103.113.8:8084/admin"'
+	BASE_API: '"http://localhost:8084/admin"'
 }
