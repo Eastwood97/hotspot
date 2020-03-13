be552ae5b0089f4e5f0cb9b7d6d6a0e1e162c4f5
huixingm <zcynewbsd@163.com>
2020-03-13 09:59:16 +0800

    提交

--- src/views/captureHistory/captureFace.vue
+++ src/views/captureHistory/captureFace.vue
@@ -57,12 +57,8 @@
             :src="picURL+scope.row.captureFaceStorageUrl"
             width="80"
             preview="1"
-<<<<<<< Updated upstream
             preview-text="抓拍人脸"
           />
-=======
-            preview-text="抓拍人脸">
->>>>>>> Stashed changes
         </template>
       </el-table-column>
 
